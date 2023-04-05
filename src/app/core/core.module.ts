d5ed3a1c294b99200b13e2668668fac390cfe662
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-04-05 22:37:17 +0200

    ST-6: Add translation module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
+import { CoreTranslationModule } from './core-translation.module';
 
 
 @NgModule({
-  imports: [CoreRoutingModule],
+  imports: [
+    CoreRoutingModule,
+    CoreTranslationModule.forRoot()
+  ],
   exports: [CoreRoutingModule]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(
+    @Optional() @SkipSelf() parentModule: CoreModule
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import available only in AppModule');
+    }
+  }
+}
