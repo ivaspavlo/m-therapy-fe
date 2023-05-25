3671e5945739f44d891d7506dcf9885515fa9159
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-05-25 20:26:02 +0200

    ST-36: Start implementing services section

---
+++ src/app/modules/main/components/services-section/services-section.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-services-section',
+  templateUrl: './services-section.component.html',
+  styleUrls: ['./services-section.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ServicesSectionComponent {
+
+}
