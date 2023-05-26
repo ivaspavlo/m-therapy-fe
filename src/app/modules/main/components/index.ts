700d7df94f82f2d401e4a023283d1a50e066a57e
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-05-26 19:04:42 +0200

    ST-37: Update styles for video bg and header

--- src/app/modules/main/components/index.ts
+++ src/app/modules/main/components/index.ts
@@ -1,12 +1,10 @@
 import { LandingSectionComponent } from './landing-section/landing-section.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ServicesSectionComponent } from './services-section/services-section.component';
-import { VideoComponent } from './video/video.component';
 
 
 export const COMPONENTS = [
   MainPageComponent,
   LandingSectionComponent,
-  VideoComponent,
   ServicesSectionComponent
 ];
