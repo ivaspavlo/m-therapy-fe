4232d27c050067e81141a167cc24b8309f012c2f
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-06-02 09:08:16 +0200

    ST-44.2: Adjust styles

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Input, Optional } from '@angular/core';
-import { ControlContainer, ControlValueAccessor, FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 
 export type InputTypes = 'text' | 'number' | 'textarea' | 'password';
@@ -10,92 +9,27 @@ export type InputTypes = 'text' | 'number' | 'textarea' | 'password';
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: InputComponent,
-    multi: true
-  }]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InputComponent implements OnInit, ControlValueAccessor {
+export class InputComponent implements OnInit {
 
-  @Input() controlName = '';
+  @Input() control: FormControl;
+  @Input() errorsMap: { [key:string]: string; };
   @Input() label = '';
   @Input() placeholder = '';
-  @Input() errorsMap: { [key:string]: string; };
-  @Input() type: InputTypes;
-  
-  get isText() { return this.type === 'text'; }
-  get isTextArea() { return this.type === 'textarea'; }
-  
-  public innerControl = new FormControl();
-  public innerInputType: InputTypes;
+  @Input() type: InputTypes = 'text';
+
+  public get isText() { return this.type === 'text'; }
+  public get isTextArea() { return this.type === 'textarea'; }
   public hasFocus = false;
-
-  // ControlContainer
-  public get form(): FormGroup { return this.controlContainer.control as FormGroup; }
-  public get control(): FormControl { return this.form.get(this.controlName) as FormControl; }
-
-  // ControlValueAccessor
-  private onChange: Function;
-  private onTouched: Function;
+  public id: string = '';
 
   private componentDestroyed$: Subject<void> = new Subject();
 
-  constructor(
-    @Optional() private controlContainer: ControlContainer
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.initInnerInputType();
-    this.listenToInnerControlChanges();
-  }
-  
-  // ControlValueAccessor methods
-  public registerOnChange(fn: Function): void {
-    this.onChange = fn;
-  }
-  
-  public registerOnTouched(fn: Function): void {
-    this.onTouched = fn;
-  }
-  
-  public writeValue(value: unknown): void {
-    this.innerControl.patchValue(value);
-  }
-  
-  // Public methods
-  public listenToInnerControlChanges(): void {
-    this.innerControl.valueChanges.pipe(
-      takeUntil(this.componentDestroyed$)
-    ).subscribe((value) => {
-      this.onInput(value);
-    });
-  }
-
-  public onFocus(): void {
-    this.onTouched();
-    this.hasFocus = true;
-  }
-
-  public onBlur(): void {
-    this.hasFocus = false;
-  }
-
-  public onInput(value: string): void {
-    let formattedValue = value;
-    this.innerControl.patchValue(formattedValue, { emitEvent: false, onlySelf: true });
-    if (this.onChange) {
-      this.onChange(formattedValue);
-    }
-  }
-  
-  public onPasswordToggle(isHidden: Event): void {
-    this.innerInputType = isHidden ? 'password' : 'text';
-  }
-  
-  // Private mehtods
-  private initInnerInputType(): void {
-    this.innerInputType = !this.type ? 'text' : this.type;
+    this.id = `${this.label}_${Math.random()}`;
   }
   
   ngOnDestroy() {
