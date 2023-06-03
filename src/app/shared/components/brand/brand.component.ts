ab497fdc0ed203344ea39acb6b0def4606a2b01e
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-06-03 15:22:41 +0200

    ST-46: Add brand component

---
+++ src/app/shared/components/brand/brand.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-brand',
+  templateUrl: './brand.component.html',
+  styleUrls: ['./brand.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BrandComponent {
+
+  @Input() hasLogo: boolean = true;
+  @Input() isClickable: boolean = true;
+
+  @Output() brandClick: EventEmitter<void> = new EventEmitter();
+
+  public onClick(): void {
+    this.brandClick.emit();
+  }
+
+}
