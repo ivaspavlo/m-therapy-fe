bd1eb62a5de917905950f4c99d79b72a59738d98
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-06-20 21:05:12 +0200

    ST-67: Implement skeleton for auth page

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,15 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
-import { CORE_ROUTE_NAMES } from '@app/core/constants';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RemindComponent } from './components/remind/remind.component';
+import { UpdateComponent } from './components/update/update.component';
 
+
+const AUTH_ROUTE_NAMES = {
+  BLANK: '',
+  LOGIN: 'login',
+  REGISTER: 'register',
+  REMIND_PASSWORD: 'remind',
+  UPDATE_PASSWORD: 'update'
+};
 
 const authRouts: Route[] = [
   {
-    path: CORE_ROUTE_NAMES.BLANK,
-    pathMatch: 'full',
-    component: AuthPageComponent
+    path: AUTH_ROUTE_NAMES.BLANK,
+    component: AuthPageComponent,
+    children: [{
+      path: AUTH_ROUTE_NAMES.BLANK,
+      pathMatch: 'full',
+      redirectTo: AUTH_ROUTE_NAMES.LOGIN
+    }, {
+      path: AUTH_ROUTE_NAMES.LOGIN,
+      component: LoginComponent
+    }, {
+      path: AUTH_ROUTE_NAMES.REGISTER,
+      component: RegisterComponent
+    }, {
+      path: AUTH_ROUTE_NAMES.REMIND_PASSWORD,
+      component: RemindComponent
+    }, {
+      path: AUTH_ROUTE_NAMES.UPDATE_PASSWORD,
+      component: UpdateComponent
+    }]
   }
 ];
 
