6701d6aecc6af9f65584ce4bf5a31d13d6b077ca
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-06-28 18:46:21 +0200

    ST-70.2: Fix animation bugs for auth module

--- src/app/modules/auth/constants/auth-routing-animations.constant.ts
+++ src/app/modules/auth/constants/auth-routing-animations.constant.ts
@@ -2,6 +2,7 @@ import { trigger, transition, style, query, animateChild, group, animate } from 
 
 
 export const AuthRoutingAnimations = trigger('authAnimate', [
+	transition('void => *', animate(0)),
 	transition('* => *', [
 		style({ position: 'relative' }),
 		query(':enter, :leave', [
@@ -13,7 +14,7 @@ export const AuthRoutingAnimations = trigger('authAnimate', [
 				height: '100vh'
 			})
 		], { optional: true }),
-		query(':enter', [style({ top: '-100vh' })]),
+		query(':enter', [style({ top: '-100vh' })], { optional: true }),
 		query(':leave', animateChild(), { optional: true }),
 		group([
 			query(':leave', [animate('0.3s ease-out', style({ top: '100vh', opacity: 0 }))], { optional: true }),
