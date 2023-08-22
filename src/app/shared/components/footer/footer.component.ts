73f02c82d2b5509f08072a7a3ba331a284b6f0a6
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-08-22 19:56:56 +0200

    MT-107: Update footer buttons

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,6 +2,8 @@ import { DOCUMENT } from '@angular/common';
 import { Component, OnInit, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { INPUT_TYPES } from '@app/core/constants';
+import { DialogService } from '@app/modules/ui/dialog';
+import { takeUntil } from 'rxjs/operators';
 
 
 @Component({
@@ -19,7 +21,8 @@ export class FooterComponent implements OnInit {
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private dialogService: DialogService
   ) { }
 
   ngOnInit(): void {
@@ -32,5 +35,20 @@ export class FooterComponent implements OnInit {
   public onClickBrand(): void {
     this.document.body.scrollTo({ top: 0, behavior: 'smooth' });
   }
+
+  public onShowTeamModal(): void {
+    const dialogConfig = {
+      test: 'TEST'
+    };
+    // this.dialogService.open(TestModalComponent, dialogConfig).afterClosed.pipe(
+    //   takeUntil(this.componentDestroyed$)
+    // ).subscribe(() => {
+    //   console.log('works');
+    // });
+  }
+
+  public onShowUpdates(): void { }
+
+  public onShowCertificates(): void { }
 
 }
