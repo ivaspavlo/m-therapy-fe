849d44275cb938ed23a3d45bbc03fd0e033966e0
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-11-11 12:07:57 +0100

    MT-116: Add translations to register, refactor brand and logo components

--- src/app/modules/lazy/auth/components/register/register.component.ts
+++ src/app/modules/lazy/auth/components/register/register.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { INPUT_TYPES } from '@app/core/constants';
-import { AuthService } from '@app/core/services';
+import { TranslateService } from '@ngx-translate/core';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { INPUT_TYPES, ToastType } from '@app/core/constants';
+import { AuthService, ToasterService } from '@app/core/services';
 import { DateValidators, PasswordValidators } from '../../constants';
 
 
@@ -12,13 +15,18 @@ import { DateValidators, PasswordValidators } from '../../constants';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent {
-
   public registerForm!: FormGroup;
   public INPUT_TYPES = INPUT_TYPES;
+  private messages = {
+    success: 'auth.success',
+    failure: 'auth.failure'
+  }
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private toasterService: ToasterService,
+    private translateService: TranslateService
   ) { }
 
   ngOnInit() {
@@ -34,7 +42,12 @@ export class RegisterComponent {
   }
 
   public onRegister(): void {
-    debugger;
-    this.authService.register(this.registerForm.value()).subscribe();
+    this.authService.register(this.registerForm.value).pipe(
+      catchError(() => of(null))
+    ).subscribe((res: any) => {
+      if (!res) {
+        this.toasterService.show(this.translateService.instant(this.messages.failure), ToastType.ERROR);
+      }
+    });
   }
 }
