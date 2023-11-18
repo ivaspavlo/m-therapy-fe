6f81d086238c2fc108dc42c3053e5188b9dfd842
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-11-18 14:08:25 +0100

    MT-122.1: Add confirm register component

---
+++ src/app/modules/lazy/auth/components/confirm-register/confirm-register.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-register',
+  templateUrl: './confirm-register.component.html',
+  styleUrls: ['./confirm-register.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfirmRegisterComponent {
+
+}
