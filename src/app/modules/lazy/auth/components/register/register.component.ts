1cea9003f116c834f7c651e2b9deb8e328bc52ca
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-11-22 18:46:28 +0100

    MT-122.2: Partially implement confirm register resolver

--- src/app/modules/lazy/auth/components/register/register.component.ts
+++ src/app/modules/lazy/auth/components/register/register.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { catchError, delay } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { INPUT_TYPES, ToastType } from '@app/core/constants';
 import { AuthService, ToasterService } from '@app/core/services';
 import { DateValidators, PasswordValidators } from '../../constants';
@@ -19,6 +20,7 @@ export class RegisterComponent {
   public INPUT_TYPES = INPUT_TYPES;
   public isLoading: boolean = false;
   public isEmailConfirmationRequired: boolean = false;
+  public isEmailConfirmed$: Observable<boolean> | null = null;
   private messages = {
     success: 'auth.success',
     failure: 'auth.failure'
@@ -29,10 +31,15 @@ export class RegisterComponent {
     private cdr: ChangeDetectorRef,
     private authService: AuthService,
     private toasterService: ToasterService,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.isEmailConfirmed$ = this.activatedRoute.data.pipe(
+      map(({ data }) => data)
+    );
+
     this.registerForm = this.fb.group({
       firstname: ['test', Validators.required],
       lastname: ['test', Validators.required],
@@ -40,14 +47,18 @@ export class RegisterComponent {
       phone: ['111222333444', [Validators.required, Validators.minLength(12)]],
       password: ['TopSecret+1',  [Validators.required, PasswordValidators.default]],
       confirmPassword: ['TopSecret+1', [Validators.required, PasswordValidators.default, PasswordValidators.passwordsEqual()]],
-      birthday: ['', [Validators.required, DateValidators.birthDate]]
+      birthday: ['', [Validators.required, DateValidators.birthDate]],
+      lang: ['']
     });
   }
 
   public onRegister(): void {
     this.isLoading = true;
-    this.authService.register(this.registerForm.value).pipe(
-      delay(3000),
+    const req = {
+      ...this.registerForm.value,
+      lang: this.translateService.currentLang
+    };
+    this.authService.register(req).pipe(
       catchError(() => of(null))
     ).subscribe((res: any) => {
       this.isLoading = false;
