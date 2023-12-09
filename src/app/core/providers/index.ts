d40dd44a10cc6f34898a78717bcb3a79a32ed70b
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-12-09 21:34:51 +0100

    MT-128: Fix auth interceptor

--- src/app/core/providers/index.ts
+++ src/app/core/providers/index.ts
@@ -3,16 +3,19 @@ import { windowProvider } from './window.provider';
 import { sessionStorageProvider } from './session-storage.provider';
 import { localStorageProvider } from './local-storage.provider';
 import { localeProvider } from './locale.provider';
+import { AuthInterceptorProvider } from './auth-interceptor.provider';
 
 
 export * from './window.provider';
 export * from './session-storage.provider';
 export * from './local-storage.provider';
 export * from './locale.provider';
+export * from './auth-interceptor.provider';
 
 export const CORE_PROVIDERS = [
   windowProvider,
   sessionStorageProvider,
   localStorageProvider,
-  localeProvider
+  localeProvider,
+  AuthInterceptorProvider
 ];
