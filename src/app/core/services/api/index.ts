f3bb20f05134c576240a196598dd8765b37be477
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-02-01 21:04:34 +0100

    MT-150: Partially connect product api

--- src/app/core/services/api/index.ts
+++ src/app/core/services/api/index.ts
@@ -1,3 +1,4 @@
 export * from './ad-api.service';
 export * from './auth-api.service';
 export * from './user-api.service';
+export * from './product.service';
--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,6 +1,7 @@
 export * from './language.interface';
 export * from './user.interface';
 export * from './header-control.interface';
+export * from './product.interface';
 
 // API interfaces
 export * from './api';
