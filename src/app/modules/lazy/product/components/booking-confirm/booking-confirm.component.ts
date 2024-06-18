c89f14fc053b19f613afc9385bb1ffc95aea6698
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-06-18 08:08:51 +0200

    MT-166.2: Implement component structure

---
+++ src/app/modules/lazy/product/components/booking-confirm/booking-confirm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-confirm',
+  templateUrl: './booking-confirm.component.html',
+  styleUrls: ['./booking-confirm.component.scss']
+})
+export class BookingConfirmComponent {
+
+}
--- src/app/modules/lazy/product/components/index.ts
+++ src/app/modules/lazy/product/components/index.ts
@@ -1,5 +1,9 @@
 import { ProductComponent } from "../product.component";
+import { BookingConfirmComponent } from "./booking-confirm/booking-confirm.component";
+import { BookingComponent } from "./booking/booking.component";
 
 export const COMPONENTS = [
-  ProductComponent
+  ProductComponent,
+  BookingComponent,
+  BookingConfirmComponent
 ];
