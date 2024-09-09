8a6909fb5cfb492863ba731c54109b36bb28814d
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-09-09 19:27:26 +0200

    MT-166.34: Implement resolver of preBooking

--- src/app/modules/lazy/product/components/booking-confirm/booking-confirm.component.ts
+++ src/app/modules/lazy/product/components/booking-confirm/booking-confirm.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { BookingApiService } from '@app/core/services';
+import { IPreBooking } from '@app/interfaces';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-booking-confirm',
   templateUrl: './booking-confirm.component.html',
-  styleUrls: ['./booking-confirm.component.scss']
+  styleUrls: ['./booking-confirm.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookingConfirmComponent {
+export class BookingConfirmComponent implements OnInit {
+  public preBooking$!: Observable<IPreBooking>;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private bookingApiService: BookingApiService
+  ) {
+    debugger;
+  }
+
+  ngOnInit(): void {
+    debugger;
+    this.preBooking$ = this.activatedRoute.data.pipe(
+      map((res: Data) => {
+        debugger;
+        return res.preBooking;
+      }),
+      shareReplay()
+    );
+  }
 }
--- src/app/modules/lazy/product/product-routing.module.ts
+++ src/app/modules/lazy/product/product-routing.module.ts
@@ -5,58 +5,62 @@ import { catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 import { BookingApiService } from '@app/core/services';
+import { IBookingSlot, IPreBooking, IResponse } from '@app/interfaces';
 import { ProductComponent } from './product.component';
-import { IBookingSlot, IResponse } from '@app/interfaces';
-import { ConfirmBookingComponent } from '../auth/components/confirm-booking/confirm-booking.component';
+import { BookingConfirmComponent } from './components';
 
 
 export enum PRODUCT_ROUTE_NAMES {
-  CONFIRM_BOOKING = 'confirm-booking:token'
+  CONFIRM_BOOKING = 'confirm-booking/:token'
 }
 
 const productRoutes: Route[] = [
   {
     path: CORE_ROUTE_NAMES.BLANK,
     component: ProductComponent,
-    children: [
-      {
-        path: '',
-        component: ProductComponent,
-        resolve: {
-          product: () => {
-            const router = inject(Router);
-            const bookingService = inject(BookingApiService);
+    resolve: {
+      product: () => {
+        const router = inject(Router);
+        const bookingService = inject(BookingApiService);
 
-            const product = router.getCurrentNavigation()?.extras?.state;
-            if (!product) {
+        const product = router.getCurrentNavigation()?.extras?.state;
+        if (!product) {
+          router.navigateByUrl(CORE_ROUTE_NAMES.HOME);
+          return;
+        }
+
+        return bookingService.getBookingSlots().pipe(
+          catchError(() => of(null)),
+          map((res: IResponse<IBookingSlot[]> | null) => {
+            if (res === null || !res?.success) {
               router.navigateByUrl(CORE_ROUTE_NAMES.HOME);
               return;
             }
-
-            return bookingService.getBookingSlots().pipe(
-              catchError(() => of(null)),
-              map((res: IResponse<IBookingSlot[]> | null) => {
-                if (res === null || !res?.success) {
-                  router.navigateByUrl(CORE_ROUTE_NAMES.HOME);
-                  return;
-                }
-                return {
-                  product,
-                  dates: res.data
-                }
-              })
-            );
-          }
-        }
+            return {
+              product,
+              dates: res.data
+            }
+          })
+        );
       }
-    ]
+    }
   }, {
     path: PRODUCT_ROUTE_NAMES.CONFIRM_BOOKING,
-    component: ConfirmBookingComponent,
+    component: BookingConfirmComponent,
     resolve: {
       preBooking: (route: ActivatedRouteSnapshot) => {
+        const router = inject(Router);
+
         return inject(BookingApiService).getPreBooking(route.paramMap.get('token')!).pipe(
-          catchError(() => of(null))
+          catchError(() => of(null)),
+          map((res: IResponse<IPreBooking> | null) => {
+            debugger
+            if (res === null || !res?.success) {
+              router.navigateByUrl(CORE_ROUTE_NAMES.HOME);
+              return;
+            }
+            return res.data;
+          })
         );
       }
     }
