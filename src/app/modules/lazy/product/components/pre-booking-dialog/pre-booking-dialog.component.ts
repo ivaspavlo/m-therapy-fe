825eba4e094a522aabe1e75e2bd24caa967d60fd
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-09-11 19:13:48 +0200

    MT-166.35: Complete post preBooking, partially implement preBooking dialog

---
+++ src/app/modules/lazy/product/components/pre-booking-dialog/pre-booking-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-booking-dialog',
+  templateUrl: './pre-booking-dialog.component.html',
+  styleUrls: ['./pre-booking-dialog.component.scss']
+})
+export class PreBookingDialogComponent {
+
+}
