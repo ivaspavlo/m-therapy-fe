6fdbb1b938b0fae40b824764e11bc89a1c16aca0
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-09-14 08:22:45 +0200

    MT-166.38: Partially implemented dialog booking-confirm

--- src/app/modules/lazy/product/components/pre-booking-dialog/pre-booking-dialog.component.ts
+++ src/app/modules/lazy/product/components/pre-booking-dialog/pre-booking-dialog.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { INPUT_TYPES } from '@app/core/constants';
 
 @Component({
   selector: 'app-pre-booking-dialog',
@@ -6,6 +8,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./pre-booking-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PreBookingDialogComponent {
+export class PreBookingDialogComponent implements OnInit {
+  public INPUT_TYPES = INPUT_TYPES;
+  public controlName: string = 'email';
+  public formGroup!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      [this.controlName]: this.fb.control('', [Validators.required, Validators.email])
+    });
+  }
+
+  public onInputSubmit(): void {
+    console.log('works');
+  }
 }
