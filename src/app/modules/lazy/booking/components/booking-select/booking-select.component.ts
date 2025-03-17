cfb58be5878dfcd26ca656030716ce290bf07e3c
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-03-17 08:31:16 +0100

    MT-181.7: Refactor booking payment component

--- src/app/modules/lazy/booking/components/booking-select/booking-select.component.ts
+++ src/app/modules/lazy/booking/components/booking-select/booking-select.component.ts
@@ -1,15 +1,14 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Data, Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { combineLatest, Observable, of } from 'rxjs';
-import { catchError, first, map, shareReplay } from 'rxjs/operators';
+import { catchError, map, shareReplay } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
-import { IBookingSlot, IContent, IProduct, IProductBooking, IResponse } from '@app/interfaces';
-import { CORE_ROUTE_NAMES, LANGUAGE, USER_EMAIL } from '@app/core/constants';
-import { BookingApiService, ContentApiService, UserManagementService } from '@app/core/services';
-import { LOCAL_STORAGE } from '@app/core/providers';
+import { IBookingSlot, IContent, IProductBooking, IResponse } from '@app/interfaces';
+import { CORE_ROUTE_NAMES, LANGUAGE } from '@app/core/constants';
+import { ContentApiService } from '@app/core/services';
 import { DestroySubscriptions } from '@app/shared/classes';
 import { BookingManagementService } from '@app/core/services/booking-management.service';
 import { BOOKING_ROUTE_NAMES } from '../../constants';
@@ -30,18 +29,16 @@ export class BookingSelectComponent extends DestroySubscriptions implements OnIn
   public selectedSlots = new Map();
 
   constructor(
-    @Inject(LOCAL_STORAGE) private localStorage: Storage,
-    private activatedRoute: ActivatedRoute,
     private fb: FormBuilder,
     private datePipe: DatePipe,
-    private bookingApiService: BookingApiService,
     private contentApiService: ContentApiService,
-    private userService: UserManagementService,
     private translateService: TranslateService,
     private router: Router,
     private bookingManagementService: BookingManagementService
   ) {
     super();
+
+    
   }
 
   ngOnInit(): void {
@@ -75,37 +72,10 @@ export class BookingSelectComponent extends DestroySubscriptions implements OnIn
 
     const bookingData = {
       bookings: [...cart?.bookings || [], currentBookings],
-      email: this.localStorage.getItem(USER_EMAIL),
       lang: this.translateService.currentLang as LANGUAGE
     }
 
-    // bookings: IProductBooking[],
-    // email?: string,
-    // comment?: string,
-    // phone?: string,
-    // lang: LANGUAGE
-
     this.router.navigateByUrl(`${CORE_ROUTE_NAMES.BOOKING}/${BOOKING_ROUTE_NAMES.BOOKING_PAYMENT}`);
-
-    // if (!this.userService.isLoggedIn) {
-    //   const bookingData = {
-    //     product,
-    //     datesSelected: Array.from(this.selectedSlots.values()),
-    //     lang: this.translateService.currentLang as LANGUAGE
-    //   };
-
-    //   this.bookingManagementService.addToCart(bookingData);
-
-    //   this.router.navigateByUrl(`${CORE_ROUTE_NAMES.BOOKING}/${BOOKING_ROUTE_NAMES.BOOKING_PAYMENT}`);
-    // }
-
-    // const req = {
-    //   email: this.localStorage.getItem(USER_EMAIL) as string,
-    //   bookingSlots: Array.from(this.selectedSlots, ([_, value]) => value),
-    //   lang: this.translateService.currentLang as LANGUAGE
-    // }
-
-    // this.bookingApiService.setPreBooking(req);
   }
 
   private initData(): void {
