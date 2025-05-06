261295618cf09dcd65ad0ba7fcb9d7e56c657d63
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-05-06 10:09:58 +0200

    MT-181.53: Implement info-block component

--- src/app/modules/ui/info-block/info-block.component.spec.ts
+++ src/app/modules/ui/info-block/info-block.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InfoComponent } from './info.component';
+import { InfoBlockComponent } from './info-block.component';
 
-describe('InfoComponent', () => {
-  let component: InfoComponent;
-  let fixture: ComponentFixture<InfoComponent>;
+describe('InfoBlockComponent', () => {
+  let component: InfoBlockComponent;
+  let fixture: ComponentFixture<InfoBlockComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [InfoComponent]
+      declarations: [InfoBlockComponent]
     });
-    fixture = TestBed.createComponent(InfoComponent);
+    fixture = TestBed.createComponent(InfoBlockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
