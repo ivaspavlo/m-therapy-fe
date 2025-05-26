14ccb8c621b1089362b702142c966b722aff9764
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-05-26 08:11:44 +0200

    MT-184.15: Add correct api url

--- src/app/core/services/api/user-api.service.ts
+++ src/app/core/services/api/user-api.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { API_URL } from '@env/environment';
+import { API_URL_FN } from '@env/environment';
 import { IUser, IResponse, ISubscribeAdEmailsReq, IUserDetails } from '@app/interfaces';
-
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +15,14 @@ export class UserApiService {
   ) { }
 
   public getUserById(): Observable<IResponse<IUser>> {
-    return this.http.get<IResponse<IUser>>(`${API_URL}/user`);
+    return this.http.get<IResponse<IUser>>(API_URL_FN('user'));
   }
 
   public subscribeAdEmails(req: ISubscribeAdEmailsReq): Observable<IResponse<ISubscribeAdEmailsReq>> {
-    return this.http.post<IResponse<ISubscribeAdEmailsReq>>(`${API_URL}/user/subscribe`, req);
+    return this.http.post<IResponse<ISubscribeAdEmailsReq>>(API_URL_FN('/user/subscribe'), req);
   }
 
   public getUserDetails(): Observable<IResponse<IUserDetails>> {
-    return this.http.get<IResponse<ISubscribeAdEmailsReq>>(`${API_URL}/user/details`);
+    return this.http.get<IResponse<ISubscribeAdEmailsReq>>(API_URL_FN('/user/details'));
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const IS_PROD = true;
 export const ENV_NAME = 'PROD';
-export const API_URL = 'https://uptracker-dev2.herokuapp.com/api/v2';
 export const API_VERSION = 'api';
+
+export const API_URL_FN = (subpath: string) => (`https://${subpath}4i4yalapna-uc.a.run.app`);
