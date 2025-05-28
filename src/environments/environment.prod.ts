38ee92201506b87eab0f38b2dc61747270a7efd3
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-05-28 08:30:35 +0200

    MT-184.17: Set up prod url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const IS_PROD = true;
 export const ENV_NAME = 'PROD';
 export const API_VERSION = 'api';
 
-export const API_URL_FN = (subpath: string) => (`https://${subpath}-4i4yalapna-uc.a.run.app`);
+export const API_URL_FN = (subpath: string) => (`https://${subpath}-7lsaj7ytxq-uc.a.run.app`);
