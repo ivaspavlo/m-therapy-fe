fecb9ec43110ea8c0d9fe4571caf184af0c256a6
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-10-22 08:30:54 +0200

    MT-187.17: Configure routing

--- src/app/modules/lazy/admin/admin-page.component.ts
+++ src/app/modules/lazy/admin/admin-page.component.ts
@@ -1,9 +1,20 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { AppRoutingAnimations } from "../auth/constants";
 
 @Component({
   selector: "app-admin-page",
   templateUrl: "./admin-page.component.html",
   styleUrls: ["./admin-page.component.scss"],
+  animations: [AppRoutingAnimations],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminPageComponent {}
+export class AdminPageComponent {
+  public prepareRoute(outlet: RouterOutlet): boolean {
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData["animationState"]
+    );
+  }
+}
